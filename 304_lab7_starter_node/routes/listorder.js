5b6023163e78af4666ef7c29fb3daaffa420c866
Carson Ricca <carsonricca28@gmail.com>
2020-11-06 20:25:43 -0800

    Initial Changes To Node Website

--- 304_lab7_starter_node/routes/listorder.js
+++ 304_lab7_starter_node/routes/listorder.js
@@ -3,9 +3,42 @@ const router = express.Router();
 const sql = require('mssql');
 const moment = require('moment');
 
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
     res.setHeader('Content-Type', 'text/html');
-    res.write('<title>YOUR NAME Grocery Order List</title>');
+    res.write('<title>Electric Lettuce Order List</title>');
+
+    (async function() {
+        try {
+            let pool = await sql.connect(dbConfig);
+
+            let sqlQuery = "SELECT * FROM product";
+            let results = await pool.request()
+                .query(sqlQuery);
+            console.dir(results)
+
+            res.write("<table>" +
+                "<tr>" +
+                    "<th>Product Name</th>" +
+                    "<th>Category ID</th>" +
+                    "<th>Product Description</th>" +
+                    "<th>Price</th>" +
+                "</tr>");
+            for(let i = 0; i<results.length; i++) {
+                let result = results[i];
+                res.write("<tr>" +
+                    "<td>" + result.productName + "</td>" +
+                    "<td>" + result.categoryId + "</td>" +
+                    "<td>" + result.productDesc + "</td>" +
+                    "<td>" + result.productPrice + "</td>" +
+                    "</tr>");
+            }
+            res.write("</table>");
+        }
+        catch (err) {
+            console.dir(err);
+            res.write(err);
+        }
+    })();
 
     /** Create connection, and validate that it connected successfully **/
 
@@ -24,8 +57,6 @@ router.get('/', function(req, res, next) {
             For each product in the order
                 Write out product information 
     **/
-
-    res.end();
 });
 
 module.exports = router;
