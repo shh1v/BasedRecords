378547c137c530dcb9576c1a4c0111ce9d384967
keyvankhademi <keyvankhademi@gmail.com>
2020-12-04 21:32:51 -0800

    Edit product

--- routes/product.js
+++ routes/product.js
@@ -100,4 +100,88 @@ router.post('/add', function (req, res, next) {
     })();
 });
 
+router.get('/:id/edit', function (req, res, next) {
+    (async function f() {
+        try {
+            const productId = req.params.id;
+
+            let pool = await sql.connect(dbConfig);
+            const warehouses = (await pool.request().query(`SELECT *
+                                                            FROM warehouse`)).recordset;
+            const categories = (await pool.request().query(`SELECT *
+                                                            FROM category`)).recordset;
+            const product = (await pool.request()
+                .input('productId', sql.Int, productId)
+                .query(
+                        `SELECT product.productId, quantity, productPrice, productName, productDesc, warehouseId, categoryId
+                         FROM product
+                                  INNER JOIN productinventory ON product.productId = productinventory.productId
+                         WHERE product.productId = @productId`
+                )).recordset[0];
+            res.render('product/edit', {
+                warehouses: warehouses,
+                categories: categories,
+                product: product,
+            });
+        } catch (err) {
+            console.dir(err);
+            res.render('message', {
+                type: 'danger',
+                message: err,
+            })
+        }
+    })();
+});
+
+router.post('/:id/edit', function (req, res, next) {
+    (async function f() {
+        try {
+            const productId = req.params.id;
+            const warehouseId = req.body.warehouse;
+            const categoryId = req.body.category;
+            const name = req.body.name;
+            const price = req.body.price;
+            const description = req.body.description;
+            const quantity = req.body.quantity;
+
+            let pool = await sql.connect(dbConfig);
+            await pool.request()
+                .input('name', sql.VarChar, name)
+                .input('price', sql.Decimal(10, 2), price)
+                .input('desc', sql.VarChar, description)
+                .input('categoryId', sql.Int, categoryId)
+                .input('productId', sql.Int, productId)
+                .query(
+                        `UPDATE product
+                         SET productName  = @name,
+                             productPrice = @price,
+                             productDesc  = @desc,
+                             categoryId   = @categoryId
+                         WHERE productId = @productId
+                    `);
+            await pool.request()
+                .input('productId', sql.Int, productId)
+                .input('warehouseId', sql.Int, warehouseId)
+                .input('quantity', sql.Int, quantity)
+                .input('price', sql.Decimal(10, 2), price)
+                .query(`UPDATE productinventory
+                        SET warehouseId = @warehouseId,
+                            quantity    = @quantity,
+                            price       = @price
+                        WHERE productId = @productID
+                `);
+            res.render('message', {
+                type: 'success',
+                message: 'Product Added Successfully',
+            });
+        } catch (err) {
+            console.dir(err);
+            res.render('message', {
+                type: 'danger',
+                message: err,
+            })
+        }
+    })();
+});
+
 module.exports = router;
