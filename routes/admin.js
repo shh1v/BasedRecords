cdf24193c6b1252089fe37dcc2562fe1a4824f93
Mohamad Agha <73283975+mo-agha@users.noreply.github.com>
2020-12-05 19:08:55 +0400

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -26,4 +26,26 @@ router.get('/', function(req, res, next) {
     }
 });
 
+router.post('/', function(req, res){
+    if(auth.checkAuthentication(req, res)) {
+        const query = "SELECT CONVERT(varchar, orderDate, 23) as dateOrdered, SUM(totalAmount) as totalAmountEarned FROM ordersummary GROUP BY CONVERT(varchar, orderDate, 23) ORDER BY CONVERT(varchar, orderDate, 23)";
+        (async function() {
+            try {
+                let pool = await sql.connect(dbConfig);
+                let result = await pool.request()
+                    .query(query);
+                res.json({
+                    sales: result.recordset
+                });
+            } catch(err) {
+                console.dir(err);
+                res.render('message', {
+                    type: 'danger',
+                    message: err,
+                })
+            }
+        })();
+    }
+});
+
 module.exports = router;
