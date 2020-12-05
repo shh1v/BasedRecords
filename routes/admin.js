7f9d4ac2f936d8745efc70e04bc8defbe7cf5db7
keyvankhademi <keyvankhademi@gmail.com>
2020-12-05 12:04:59 -0800

    Fix charts

--- routes/admin.js
+++ routes/admin.js
@@ -11,31 +11,20 @@ router.get('/', function(req, res, next) {
                 let pool = await sql.connect(dbConfig);
                 let result = await pool.request()
                     .query(query);
+
+                const sales = result.recordset;
+                let chartLabels = [];
+                let chartData = [];
+                for(let i = 0; i < sales.length; i++){
+                    chartLabels.push(sales[i].dateOrdered);
+                    chartData.push(sales[i].totalAmountEarned);
+                }
+
                 res.render('admin', {
                     title: 'Admin Sales Report',
-                    sales: result.recordset
-                });
-            } catch(err) {
-                console.dir(err);
-                res.render('message', {
-                    type: 'danger',
-                    message: err,
-                })
-            }
-        })();
-    }
-});
-
-router.post('/', function(req, res){
-    if(auth.checkAuthentication(req, res)) {
-        const query = "SELECT CONVERT(varchar, orderDate, 23) as dateOrdered, SUM(totalAmount) as totalAmountEarned FROM ordersummary GROUP BY CONVERT(varchar, orderDate, 23) ORDER BY CONVERT(varchar, orderDate, 23)";
-        (async function() {
-            try {
-                let pool = await sql.connect(dbConfig);
-                let result = await pool.request()
-                    .query(query);
-                res.json({
-                    sales: result.recordset
+                    sales: sales,
+                    chartLabels: JSON.stringify(chartLabels),
+                    chartData: JSON.stringify(chartData),
                 });
             } catch(err) {
                 console.dir(err);
